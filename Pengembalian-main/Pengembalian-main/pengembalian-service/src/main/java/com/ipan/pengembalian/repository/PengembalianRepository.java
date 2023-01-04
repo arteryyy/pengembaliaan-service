3b57fb95b3e41040947cba1a69ba0e2a4c39a503
IrvanYuatama <113502442+arteryyy@users.noreply.github.com>
2023-01-04 18:44:39 +0700

    Add files via upload

---
+++ Pengembalian-main/Pengembalian-main/pengembalian-service/src/main/java/com/ipan/pengembalian/repository/PengembalianRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ipan.pengembalian.repository;
+
+import com.ipan.pengembalian.entity.Pengembalian;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ASUS
+ */
+@Repository
+public interface PengembalianRepository extends JpaRepository<Pengembalian, Long>{
+
+    public Pengembalian findByPengembalianId(Long pengembalianId);
+
+    
+}
